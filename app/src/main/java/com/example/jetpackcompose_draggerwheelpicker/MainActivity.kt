dbec287d8c35769ae43044fef9f5e4a05a3fca58
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-02 14:08:09 +0900

    feat: 초기 위치값 설정하는 파라미터 추가

--- app/src/main/java/com/example/jetpackcompose_draggerwheelpicker/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_draggerwheelpicker/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.jetpackcompose_draggerwheelpicker
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -12,6 +13,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -44,7 +46,7 @@ class MainActivity : ComponentActivity() {
                         pickerMaxHeight = 250.dp,
                         defaultTextStyle = MaterialTheme.typography.body1,
                         centerTextStyle = MaterialTheme.typography.h6,
-                        lists = lists
+                        lists = lists,
                     )
                 }
             }
@@ -57,6 +59,7 @@ class MainActivity : ComponentActivity() {
 fun WheelPicker(
     modifier: Modifier,
     pickerMaxHeight: Dp = 250.dp,
+    initIndex: Int? = null,
     lists: ArrayList<Int>,
     defaultTextStyle: androidx.compose.ui.text.TextStyle = MaterialTheme.typography.body1,
     centerTextStyle: androidx.compose.ui.text.TextStyle = MaterialTheme.typography.h6,
@@ -78,7 +81,7 @@ fun WheelPicker(
      *
      * */
 
-    val lazyListState = rememberLazyListState(initialFirstVisibleItemIndex = (Int.MAX_VALUE / 2.0).toInt() - 23)
+    val lazyListState = rememberLazyListState(initialFirstVisibleItemIndex = (Int.MAX_VALUE / 2.0).toInt() - 25)
     val layoutInfo: LazyListSnapperLayoutInfo = rememberLazyListSnapperLayoutInfo(lazyListState) // wheel picker 의 layout 정보
     var currentIndex: Int? // 현재 선택된 아이템의 인덱스
 
@@ -136,5 +139,18 @@ fun WheelPicker(
                 .height(pickerMaxHeight * 0.215f)
                 .background(selectedBackgroundColor)
         )
+
+        LaunchedEffect(Unit) {
+            initIndex?.let {
+                lazyListState.scrollToItem(
+                    index = (Int.MAX_VALUE / 2.0).toInt() - 25 + it,
+                    scrollOffset = lazyListState.layoutInfo.visibleItemsInfo[0].size / 5
+                )
+            } ?: kotlin.run {
+                lazyListState.scrollBy(
+                    value = lazyListState.layoutInfo.visibleItemsInfo[0].size.toFloat() / 5
+                )
+            }
+        }
     }
 }
