aa93bdf215a82388ef1689b8243b9fd74b2cccc6
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-02 13:42:56 +0900

    feat: 센터에 있는 아이템이 돋보이게, 위 아래 아이템 scale 및 색상 조정

--- app/src/main/java/com/example/jetpackcompose_draggerwheelpicker/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_draggerwheelpicker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcompose_draggerwheelpicker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -11,15 +12,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose_draggerwheelpicker.ui.theme.JetpackCompose_DraggerWheelPickerTheme
 import dev.chrisbanes.snapper.ExperimentalSnapperApi
+import dev.chrisbanes.snapper.LazyListSnapperLayoutInfo
+import dev.chrisbanes.snapper.rememberLazyListSnapperLayoutInfo
 import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
+import kotlinx.coroutines.delay
+import kotlin.math.abs
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,6 +67,8 @@ fun WheelPicker(
     val lazyListState = rememberLazyListState(
         initialFirstVisibleItemIndex = Int.MAX_VALUE / 2 - 25,
     )
+    val layoutInfo: LazyListSnapperLayoutInfo = rememberLazyListSnapperLayoutInfo(lazyListState)
+    var currentIndex: Int?
 
     Box(
         modifier = modifier,
@@ -79,13 +89,30 @@ fun WheelPicker(
             ) {
                 val index = it % lists.size
 
+                currentIndex = layoutInfo.currentItem?.index?.minus(Int.MAX_VALUE / 2)?.plus(23)?.rem(lists.size)
+
+                if (currentIndex != null && currentIndex!! < 0) {
+                    currentIndex = currentIndex!! + lists.size
+                }
+
+                val curTextIsCenter = currentIndex == lists[index].toInt()
+                val curTextIsCenterDiffer1 = currentIndex == abs(lists[index].toInt() - 1) || currentIndex == abs(lists[index].toInt() + 1)
+                val curTextIsCenterDiffer2 = currentIndex == abs(lists[index].toInt() - 2) || currentIndex == abs(lists[index].toInt() + 2)
+
                 Text(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical = 14.dp),
-                    text = lists[index],
-                    style = MaterialTheme.typography.h5,
-                    color = Color.Black,
+                        .height(pickerMaxHeight * 0.215f)
+                        .padding(vertical = 14.dp)
+                        .align(Alignment.Center)
+                        .scale(
+                            scaleX = if (curTextIsCenter) 1.2f else if (curTextIsCenterDiffer1) 1.0f else 0.8f,
+                            scaleY = if (curTextIsCenter) 1.2f else if (curTextIsCenterDiffer1) 1.0f else 0.8f
+                        )
+                    ,
+                    text = if (lists[index].toInt() == 0) "표시안함" else "${lists[index]}년",
+                    style = MaterialTheme.typography.h6,
+                    color = if (curTextIsCenter) Color.Black else if (curTextIsCenterDiffer1) Color.Black.copy(alpha = 0.3f) else Color.Black.copy(alpha = 0.1f),
                     textAlign = TextAlign.Center
                 )
 
